c88050217739474e77ba7bb246a6c0e8bec0106d
RahafAwwad <74986945+RahafAwwad@users.noreply.github.com>
2022-06-06 06:17:03 +0300

    add firebase connection

--- app/src/main/java/com/example/reaya_project/MainActivity.java
+++ app/src/main/java/com/example/reaya_project/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.reaya_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_logo);
+=======
+        setContentView(R.layout.activity_sign_in);
+        EditText username = findViewById(R.id.mass);
+        
+>>>>>>> Stashed changes
     }
 }
