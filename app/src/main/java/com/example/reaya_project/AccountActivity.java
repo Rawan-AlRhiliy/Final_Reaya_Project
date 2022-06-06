3978933c09d54b3f58e376727b083f044eec6b01
RahafAwwad <74986945+RahafAwwad@users.noreply.github.com>
2022-06-06 05:21:18 +0300

    Rahaf's first trial

--- app/src/main/java/com/example/reaya_project/AccountActivity.java
+++ app/src/main/java/com/example/reaya_project/AccountActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AccountActivity extends AppCompatActivity {
-
+//rahaf trial
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
